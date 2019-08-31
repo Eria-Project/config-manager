f4a1552f29781ab6312ebdc56f358b6d1f59e4f4
Ferllings <ferllings@gmail.com>
2019-08-31 10:26:09 +0200

    Restructured folder for modules compliance

--- eria.go
+++ eria.go
@@ -1,4 +1,4 @@
-package helpers
+package eria
 
 import (
 	"flag"
@@ -10,24 +10,24 @@ import (
 
 	configmanager "github.com/project-eria/eria-base/config-manager"
 	"github.com/project-eria/logger"
-	"github.com/project-eria/xaal-go/engine"
+	"github.com/project-eria/xaal-go"
 )
 
 var _xAALConfigFile = "xaal.json"
 
-// InitEngine load the xAAL config file and start the engine
+// InitEngine load the xAAL config file and init the engine
 func InitEngine() {
-	var config engine.XaalConfig
+	var config xaal.XaalConfig
 
 	configManagerXAAL, err := configmanager.Init(_xAALConfigFile, &config)
 	if err != nil {
-		logger.Module("engine").WithError(err).WithField("filename", _xAALConfigFile).Fatal()
+		logger.Module("xaal:engine").WithError(err).WithField("filename", _xAALConfigFile).Fatal()
 	}
 
 	if err := configManagerXAAL.Load(); err != nil {
-		logger.Module("engine").WithError(err).Fatal()
+		logger.Module("xaal:engine").WithError(err).Fatal()
 	}
-	engine.Init(config)
+	xaal.Init(config)
 }
 
 // AddShowVersion add a flag to display app version
@@ -59,4 +59,25 @@ func WaitForExit() {
 	// Block until keyboard interrupt is received.
 	<-c
 	runtime.Goexit()
+}
+
+// LoadConfig Loads the config file into a struct
+func LoadConfig(fileName string, config interface{}) *configmanager.ConfigManager {
+	cm, err := configmanager.Init(fileName, config)
+	if err != nil {
+		if configmanager.IsFileMissing(err) {
+			err = cm.Save()
+			if err != nil {
+				logger.Module("main").WithField("filename", fileName).Fatal(err)
+			}
+			logger.Module("main").Fatal("JSON Config file do not exists, created...")
+		} else {
+			logger.Module("main").WithField("filename", fileName).Fatal(err)
+		}
+	}
+
+	if err := cm.Load(); err != nil {
+		logger.Module("main").Fatal(err)
+	}
+	return cm
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/project-eria/eria-base/config-manager
+module github.com/project-eria/eria-base
 
 go 1.12
 
@@ -6,5 +6,6 @@ require (
 	github.com/fsnotify/fsnotify v1.4.7
 	github.com/ghodss/yaml v1.0.0
 	github.com/project-eria/logger v0.0.1
+	github.com/project-eria/xaal-go v0.5.0
 	github.com/tidwall/gjson v1.3.2
 )
